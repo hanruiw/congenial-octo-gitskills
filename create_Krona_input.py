09579bed5f404cc7d4698da47e99e3f0fb1e2fa1
Hanrui Wu <hwu@fredhutch.org>
2020-12-01 22:44:36 -0800

    test

--- create_Krona_input.py
+++ create_Krona_input.py
@@ -5,7 +5,6 @@ def read_PathSeq_corrected(input_file,output_file):
     output=open(output_file,'w')
     n=0    
 
-
     for each_line in pathseq:
         each_line=each_line.rstrip('\n')
         if not n==0:
@@ -21,8 +20,7 @@ def read_PathSeq_corrected(input_file,output_file):
     print("there are ", len(list_to_store_int_tax))
     pathseq.close()
     k=0
-    q=0
-    
+#    q=0
     pathseq=open(input_file,'r')
     for each_line2 in pathseq:
 
@@ -31,33 +29,26 @@ def read_PathSeq_corrected(input_file,output_file):
                 
             each_line_list=each_line2.split('\t')
 
-            if 1==1:
-                tax=each_line_list[1]
-                tax_list=tax.split('|')
+            tax=each_line_list[1]
+            tax_list=tax.split('|')
 
+            unamb_reads=int(each_line_list[-2])
 
-                unamb_reads=int(each_line_list[-2])
+#            orig_read=unamb_reads
+            if unamb_reads == 0:
+                continue
+            for each_clay in list_to_store_int_tax:
+                if (len(tax_list) == len(each_clay[:-1])-1) and (tax_list==each_clay[:len(tax_list)]):
+                    unamb_reads=unamb_reads-each_clay[-1]
 
-                orig_read=unamb_reads
-                if unamb_reads == 0:
-                    continue
-                for each_clay in list_to_store_int_tax:
-                
-                    if (len(tax_list) == len(each_clay[:-1])-1) and (tax_list==each_clay[:len(tax_list)]):
-
-                        unamb_reads=unamb_reads-each_clay[-1]
-
-                write_line=str(unamb_reads)+'\t'+'\t'.join(tax_list)
-                output.write(write_line)
-                output.write('\n')
-
+            write_line=str(unamb_reads)+'\t'+'\t'.join(tax_list)
+            output.write(write_line)
+            output.write('\n')
 
         k+=1
     pathseq.close()
     return
         
-
-
 input_file=[
     "/pathseq/1N.pathseq.txt.csv"
 ]
@@ -65,7 +56,6 @@ input_file=[
 output_file=[
     "/krona/1N.krona"
 ]
-
 
 
 for n in range(len(input_file)):
